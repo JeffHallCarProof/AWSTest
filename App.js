36d241b8998ed220ff2e1ed4077ce34bb3c366c5
JeffHallCarProof <JHall@carproof.com>
2018-09-17 15:06:57 -0400

    Screen Transitions

--- App.js
+++ App.js
@@ -9,14 +9,15 @@ import {
   TouchableHighlight,
   View,
 } from 'react-native';
-import { WebBrowser, Constants } from 'expo';
+import { createStackNavigator } from 'react-navigation';
+import { WebBrowser } from 'expo';
 import Amplify from 'aws-amplify';
 
 import amplify from './src/aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native';
 
 Amplify.configure(amplify);
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
@@ -27,7 +28,7 @@ export default class App extends React.Component {
             <TouchableHighlight
               underlayColor={'maroon'}
               style={styles.button}
-              onPress={this._onPress}
+              onPress={this._yesPress}
             >
               <Text style={styles.btext}> Girafe Facts </Text>
             </TouchableHighlight>
@@ -36,27 +37,44 @@ export default class App extends React.Component {
             <TouchableHighlight
               underlayColor={'maroon'}
               style={styles.button}
-              onPress={this._noPress}
+              onPress={() => this.props.navigation.navigate('No')}
             
             >
-              <Text style={styles.btext}> No </Text>
+              <Text style={styles.btext}> No Facts </Text>
             </TouchableHighlight>
             </View>
           </View>
 
     );
   }
-  _onPress = () => {
+  _yesPress = () => {
     Expo.WebBrowser.openBrowserAsync(
       'https://www.reddit.com/r/giraffefacts/'
     );
   };
-  _noPress = () => {
-      Expo.WebBrowser.openBrowserAsync(
-        'https://www.reddit.com/r/giraffefacts/'
-      );
-    
-  };
+}
+class noScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    No: noScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
 }
 const styles = StyleSheet.create({
   container: {
@@ -72,8 +90,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 10,
-    borderRadius: 400
+    paddingHorizontal: 10
   },
   btext: {
     color: 'white',
