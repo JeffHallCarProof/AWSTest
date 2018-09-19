f7ededf3ea4e3f3880822a5ace8ffbb712758390
Jeremy Leworthy <36454342+JeremyLeworthy@users.noreply.github.com>
2018-09-19 14:34:12 -0400

    Changed spacing

--- screens/pScreen.js
+++ screens/pScreen.js
@@ -18,57 +18,72 @@ import {
 
   //preferences screen
   export default class pScreen extends React.Component {
+
     static navigationOptions = {
       header: null,
       gesturesEnabled: false,
     };
+
     state = {
         value: 0.01
     };
+
     render() {
+
       const { navigation } = this.props;
       const screenId = navigation.getParam('sId', 'Invalid');
+
       return (
         <View style={styles.container}>
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
             <Text style={styles.textS}>ID: {JSON.stringify(screenId)}</Text>
             <Text style={styles.textS}>P: {JSON.stringify(Path)}</Text>
             <Text style={styles.textS}>bP: {JSON.stringify(bPath)}</Text>
+
           </View>
 
             <View style={styles.sContainer}>
+                
                 <Slider
-                value={this.state.value}
-                onValueChange={value => this.setState({ value })}/>
+                    value={this.state.value}
+                    onValueChange={value => this.setState({ value })}/>
 
                 <View style={styles.sliderF}>
                     <Text style={styles.containerProducts}>Least</Text>
                     <Text style={styles.containerProducts}>Most</Text>
                 </View>
-            </View>      
+
+            </View>    
+
           <Text>Value: {this.state.value}</Text>
+
           <TouchableHighlight
             underlayColor={'#0018A8'}
             style={styles.button}
             onPress={() => {
-            this.props.navigation.navigate(bPath, {
-            sId: screenId,});
+                this.props.navigation.navigate(bPath, {sId: screenId});
             }}>
+
             <Text style={styles.btext}>Go Back</Text>
+
           </TouchableHighlight>
-      </View>      
+        </View>      
       ); //End of return
     } //End of render
    
   } //End of class
 
+  //Component css
   const styles = StyleSheet.create({
+
     container: {
       flex: 1,
       backgroundColor: 'white',
       paddingHorizontal: 10,
       justifyContent: 'center'
     },
+
     sContainer: {
         flex: 1,
         marginLeft: 10,
@@ -76,9 +91,11 @@ import {
         alignItems: "stretch",
         justifyContent: "center"
     },
+
     contentContainer: {
-      paddingTop: 30,
+      paddingTop: 30
     },
+
     bcontainer: {
         flex: 1,
         backgroundColor: 'white',
@@ -87,24 +104,28 @@ import {
         paddingHorizontal: 10,
         flexDirection: 'row'
       },
+
       btext: {
-        color: 'white',
+        color: 'white'
       },
+
       button: {
         alignItems: 'center',
         backgroundColor: '#0247FE',
         padding: 10,
         borderRadius: 400
       },
+
       sliderF: {
         flexDirection:'row',
         justifyContent : 'space-between'
       },
+
       containerProducts: {
         paddingTop: 40,
         paddingLeft: 15,
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        },
+        justifyContent: 'space-between'
+      }
       
   });
