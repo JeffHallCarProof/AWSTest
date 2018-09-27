aea52a49b8b564f5f879217076d1c2a0586db969
Jeremy Leworthy <36454342+JeremyLeworthy@users.noreply.github.com>
2018-09-27 11:00:26 -0400

    Changed spacing and styling for results screen

--- screens/resultsScreen.js
+++ screens/resultsScreen.js
@@ -19,7 +19,8 @@ import { WebBrowser } from 'expo';
 import Amplify from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 
-const width = 375
+  const width = 375
+
   //results screen
   export default class resultsScreen extends React.Component {
 
@@ -39,9 +40,9 @@ const width = 375
     }
  
     UpdateProgressBar = (progress) =>
-      {
-        this.setState({ progress_count: Math.abs( progress.nativeEvent.contentOffset.x / ( this.scrollViewContent_width - this.scrollView_width ))});
-      }
+    {
+      this.setState({ progress_count: Math.abs( progress.nativeEvent.contentOffset.x / ( this.scrollViewContent_width - this.scrollView_width ))});
+    }
 
     static navigationOptions = {
       header: null,
@@ -50,57 +51,61 @@ const width = 375
 
     render() {
 
-        const { navigation } = this.props;
+      const { navigation } = this.props;
   
-        return (
+      return (
             
-            <View style={styles.bcontainer}>
-            <Text> </Text>
-            <Text> </Text>
-               <Text>Results Screen</Text>
-                <ScrollView horizontal={true} backgroundColor= '#246ee5' pagingEnabled={true} 
-                onContentSizeChange = {( width, height ) => { this.scrollViewContent_width = width }} 
-                onScroll = { this.UpdateProgressBar } 
-                onLayout = {(event) => this.scrollView_width = ( event.nativeEvent.layout.width )} 
-                scrollEventThrottle = { 12 } >
-                    <View style={styles.colContainer}><Text> Column 1 </Text><Text> Column 1 </Text><Text> Column 1 </Text></View>
-                    <View style={styles.colContainer}><Text> Column 2 </Text></View> 
-                    <View style={styles.colContainer}><Text> Column 3 </Text></View>
-                </ScrollView>
-                <View style = { styles.ProgressBar_HolderView }>
-                {   
-                    ( Platform.OS === 'android' )
-                    ?
-                        (
-                            <ProgressBarAndroid
-                              styleAttr = "Horizontal"
-                              progress = { this.state.progress_count }
-                              color = "#fff"
-                              indeterminate = { false }
-                              style = {{ width: '100%' }}
-                            />
-                        )
-                    :
-                        (
-                            <ProgressViewIOS
-                              progressTintColor = "#fff"
-                              style = {{ width: '100%' }}
-                              progress = { this.state.progress_count }
-                            />
-                        )
-                }
-                    <Text style = { styles.Percentage }> { Math.round( this.state.progress_count * 100 ) }% </Text>
-                </View>
-                <View paddingBottom={75} paddingTop={20}>
-                <TouchableHighlight
-                    underlayColor={'#0018A8'}
-                    style={styles.button}
-                    onPress={() => {this.props.navigation.navigate('Home');
-                    }}>
-                    <Text style={styles.btext}>Home</Text>
-                </TouchableHighlight>
-                  </View> 
-            </View>
+        <View style={styles.bcontainer}>
+          <View style={styles.textView}>
+            <Text style={styles.titleText}>Results Screen</Text>
+          </View>
+               
+          <ScrollView horizontal={true} backgroundColor= '#246ee5' pagingEnabled={true} 
+            onContentSizeChange = {( width, height ) => { this.scrollViewContent_width = width }} 
+            onScroll = { this.UpdateProgressBar } 
+            onLayout = {(event) => this.scrollView_width = ( event.nativeEvent.layout.width )} 
+            scrollEventThrottle = { 12 } showsHorizontalScrollIndicator={false}
+          >
+            <View style={styles.colContainer}><Text style={styles.colText}> Column 1 </Text><Text style={styles.colText}> Column 1 </Text><Text style={styles.colText}> Column 1 </Text></View>
+            <View style={styles.colContainer}><Text style={styles.colText}> Column 2 </Text></View> 
+            <View style={styles.colContainer}><Text style={styles.colText}> Column 3 </Text></View>
+          </ScrollView>
+
+          <View style = { styles.ProgressBar_HolderView }>
+          {   
+            ( Platform.OS === 'android' )
+            ?
+              (
+                <ProgressBarAndroid
+                  styleAttr = "Horizontal"
+                  progress = { this.state.progress_count }
+                  color = "#fff"
+                  indeterminate = { false }
+                  style = {{ width: '100%' }}
+                />
+              )
+            :
+              (
+                <ProgressViewIOS
+                  progressTintColor = "#fff"
+                  style = {{ width: '100%' }}
+                  progress = { this.state.progress_count }
+                />
+              )
+          }
+            <Text style = { styles.Percentage }> { Math.round( this.state.progress_count * 100 ) }% </Text>
+          </View>
+
+          <View paddingBottom={75} paddingTop={20}>
+            <TouchableHighlight
+              underlayColor={'#0018A8'}
+              style={styles.button}
+              onPress={() => {this.props.navigation.navigate('Home');}}
+            >
+              <Text style={styles.btext}>Home</Text>
+            </TouchableHighlight>
+          </View> 
+        </View>
 
         ); //End of return
     } //End of render
@@ -141,7 +146,7 @@ const styles = StyleSheet.create({
       flexDirection: 'column',
       justifyContent: 'space-evenly',
       borderColor: '#FFF',
-      borderWidth: 1,
+      borderWidth: 5,
       width: 375
     },
 
@@ -150,8 +155,7 @@ const styles = StyleSheet.create({
       justifyContent: 'space-between'
     },
 
-    ProgressBar_HolderView:
-    {
+    ProgressBar_HolderView: {
       justifyContent: 'center',
       flexDirection: 'row',
       alignItems: 'center',
@@ -165,11 +169,28 @@ const styles = StyleSheet.create({
       height: 50,
     },
 
-    Percentage:
-    {
+    Percentage: {
         position: 'absolute',
         right: 6,
         fontWeight: 'bold',
         color: 'white'
+    },
+
+    colText: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontSize: 18
+    },
+
+    titleText: {
+      fontWeight: 'bold',
+      fontSize: 20,
+      textAlign: 'center',
+    },
+
+    textView: {
+      paddingTop: 50,
+      paddingBottom: 20
     }
+    
 });
