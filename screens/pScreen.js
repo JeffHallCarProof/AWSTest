22bfc1100f62f9b5cbb1a3a91b20b765dcb2df6b
Jeremy Leworthy <36454342+JeremyLeworthy@users.noreply.github.com>
2018-10-01 13:58:07 -0400

    Spacing changes

--- screens/pScreen.js
+++ screens/pScreen.js
@@ -10,55 +10,57 @@ import {
     TouchableHighlight,
     View,
   } from 'react-native';
-  import renderIf from '../components/renderIf';
+import renderIf from '../components/renderIf';
 import { ListItem, CheckBox, Slider } from 'react-native-elements';
 import { WebBrowser } from 'expo';
 import Amplify from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import _, {debounce} from 'lodash';
+
   //preferences screen
   export default class pScreen extends React.Component {
-
 
     static navigationOptions = {
       header: null,
       gesturesEnabled: false,
     };
+
     state = {
       text: false,
-        p1: false,
-        v1: 0,
-        p2: false,
-        v2: 0,
-        p3: false,
-        v3: 0,
-        p4: false,
-        v4: 0,
-        p5: false,
-        v5: 0,
-        p6: false,
-        v6: 0,
-        p7: false,
-        v7: 0,
-        p8: false,
-        v8: 0,
-        p9: false,
-        v9: 0,
-        p10: false,
-        v10: 0,
-        p11: false,
-        v11: 0,
-        p12: false,
-        v12: 0,
-        value: 0.01,
-        disabled: false
-      }
+      p1: false,
+      v1: 0,
+      p2: false,
+      v2: 0,
+      p3: false,
+      v3: 0,
+      p4: false,
+      v4: 0,
+      p5: false,
+      v5: 0,
+      p6: false,
+      v6: 0,
+      p7: false,
+      v7: 0,
+      p8: false,
+      v8: 0,
+      p9: false,
+      v9: 0,
+      p10: false,
+      v10: 0,
+      p11: false,
+      v11: 0,
+      p12: false,
+      v12: 0,
+      value: 0.01,
+      disabled: false
+    }
 
     render() {
 
       const { navigation } = this.props;
       this.state.disabled = navigation.getParam('disabled', false);
       const screenId = navigation.getParam('sId', 'Invalid');
+
       if(JSON.stringify(screenId)==0){
         Path ='Login'
         bPath ='Questions'
@@ -71,16 +73,18 @@ import _, {debounce} from 'lodash';
 
       return (
         <View style={styles.container}>
+
         <View style={styles.tContainer}>
           <Text>Preference Assesment</Text>
         </View>
+
           <View style={styles.bcontainer}>
             <TouchableHighlight
                 selected={this.state.p1}
                 selectedN={this.state.v1}
                 underlayColor={'#0018A8'}
-              style={[styles.button, this.state.p1 && styles.buttonS]}
-              onPress={() => this.setState({ p1: !this.state.p1,
+                style={[styles.button, this.state.p1 && styles.buttonS]}
+                onPress={() => this.setState({ p1: !this.state.p1,
                 value: this.state.v1,
                 text: !this.state.p1,
                 p2: false,
@@ -90,15 +94,15 @@ import _, {debounce} from 'lodash';
                 p6: false,
                 p7: false,
                 p8: false})}>
-              <Text style={styles.btext}> Comfort </Text>
+                <Text style={styles.btext}> Comfort </Text>
             </TouchableHighlight>
             <Text> </Text>
             <TouchableHighlight
                 selected={this.state.p2}
                 selectedN={this.state.v2}
                 underlayColor={'#0018A8'}
-              style={[styles.button, this.state.p2 && styles.buttonS]}
-              onPress={() => this.setState({ p2: !this.state.p2,
+                style={[styles.button, this.state.p2 && styles.buttonS]}
+                onPress={() => this.setState({ p2: !this.state.p2,
                 value: this.state.v2,
                 text: !this.state.p2,
                 p1: false,
@@ -108,15 +112,15 @@ import _, {debounce} from 'lodash';
                 p6: false,
                 p7: false,
                 p8: false})}>
-              <Text style={styles.btext}> Environment </Text>
+                <Text style={styles.btext}> Environment </Text>
             </TouchableHighlight>
             <Text> </Text>
             <TouchableHighlight
                 selected={this.state.p3}
                 selectedN={this.state.v3}
                 underlayColor={'#0018A8'}
-              style={[styles.button, this.state.p3 && styles.buttonS]}
-              onPress={() => this.setState({ p3: !this.state.p3,
+                style={[styles.button, this.state.p3 && styles.buttonS]}
+                onPress={() => this.setState({ p3: !this.state.p3,
                 value: this.state.v3,
                 text: !this.state.p3,
                 p2: false,
@@ -126,7 +130,7 @@ import _, {debounce} from 'lodash';
                 p6: false,
                 p7: false,
                 p8: false})}>
-              <Text style={styles.btext}> Fuel Eco </Text>
+                <Text style={styles.btext}> Fuel Eco </Text>
             </TouchableHighlight>
             </View>
             
@@ -135,8 +139,8 @@ import _, {debounce} from 'lodash';
                 selected={this.state.p4}
                 selectedN={this.state.v4}
                 underlayColor={'#0018A8'}
-              style={[styles.button, this.state.p4 && styles.buttonS]}
-              onPress={() => this.setState({ p4: !this.state.p4,
+                style={[styles.button, this.state.p4 && styles.buttonS]}
+                onPress={() => this.setState({ p4: !this.state.p4,
                 value: this.state.v4,
                 text: !this.state.p4,
                 p2: false,
@@ -146,15 +150,15 @@ import _, {debounce} from 'lodash';
                 p6: false,
                 p7: false,
                 p8: false})}>
-              <Text style={styles.btext}> Reliability </Text>
+                <Text style={styles.btext}> Reliability </Text>
             </TouchableHighlight>
             <Text> </Text>
             <TouchableHighlight
                 selected={this.state.p5}
                 v5={this.state.v5}
                 underlayColor={'#0018A8'}
-              style={[styles.button, this.state.p5 && styles.buttonS]}
-              onPress={() => this.setState({ p5: !this.state.p5,
+                style={[styles.button, this.state.p5 && styles.buttonS]}
+                onPress={() => this.setState({ p5: !this.state.p5,
                 value: this.state.v5,
                 text: !this.state.p5,
                 p2: false,
@@ -164,15 +168,15 @@ import _, {debounce} from 'lodash';
                 p6: false,
                 p7: false,
                 p8: false})}>
-              <Text style={styles.btext}> Safety </Text>
+                <Text style={styles.btext}> Safety </Text>
             </TouchableHighlight>
             <Text> </Text>
             <TouchableHighlight
                 selected={this.state.p6}
                 selectedN={this.state.v6}
                 underlayColor={'#0018A8'}
-              style={[styles.button, this.state.p6 && styles.buttonS]}
-              onPress={() => this.setState({ p6: !this.state.p6,
+                style={[styles.button, this.state.p6 && styles.buttonS]}
+                onPress={() => this.setState({ p6: !this.state.p6,
                 value: this.state.v6,
                 text: !this.state.p6,
                 p2: false,
@@ -182,16 +186,17 @@ import _, {debounce} from 'lodash';
                 p1: false,
                 p7: false,
                 p8: false})}>
-              <Text style={styles.btext}> Style </Text>
+                <Text style={styles.btext}> Style </Text>
             </TouchableHighlight>
             </View>
+
             <View style={styles.bcontainer}>
             <TouchableHighlight
                 selected={this.state.p7}
                 selectedN={this.state.v7}
                 underlayColor={'#0018A8'}
-              style={[styles.button, this.state.p7 && styles.buttonS]}
-              onPress={() => this.setState({ p7: !this.state.p7,
+                style={[styles.button, this.state.p7 && styles.buttonS]}
+                onPress={() => this.setState({ p7: !this.state.p7,
                 value: this.state.v7,
                 text: !this.state.p7,
                 p2: false,
@@ -201,15 +206,15 @@ import _, {debounce} from 'lodash';
                 p6: false,
                 p1: false,
                 p8: false})}>
-              <Text style={styles.btext}> Performance </Text>
+                <Text style={styles.btext}> Performance </Text>
             </TouchableHighlight>
             <Text> </Text>
             <TouchableHighlight
                 selected={this.state.p8}
                 selectedN={this.state.v8}
                 underlayColor={'#0018A8'}
-              style={[styles.button, this.state.p8 && styles.buttonS]}
-              onPress={() => this.setState({ p8: !this.state.p8,
+                style={[styles.button, this.state.p8 && styles.buttonS]}
+                onPress={() => this.setState({ p8: !this.state.p8,
                 value: this.state.v8,
                 text: !this.state.p8,
                 p2: false,
@@ -219,76 +224,92 @@ import _, {debounce} from 'lodash';
                 p6: false,
                 p7: false,
                 p1: false})}>
-              <Text style={styles.btext}> Cargo Space </Text>
+                <Text style={styles.btext}> Cargo Space </Text>
             </TouchableHighlight>
             </View>
+            
             <View style={styles.sContainer}>
+
             {renderIf(this.state.p1, 
                 <Slider
                   value={this.state.value}
-                    onSlidingComplete={value => this.setState({ v1: value,})}/>
-                )}
-                            {renderIf(this.state.p2, 
+                  onSlidingComplete={value => this.setState({ v1: value,})}
+                />
+            )}
+
+            {renderIf(this.state.p2, 
                 <Slider
                   value={this.state.value}
-                    onSlidingComplete={value => this.setState({ v2: value,})}/>
-                )}
-                            {renderIf(this.state.p3, 
+                  onSlidingComplete={value => this.setState({ v2: value,})}
+                />
+            )}
+
+            {renderIf(this.state.p3, 
                 <Slider
                   value={this.state.value}
-                    onSlidingComplete={value => this.setState({ v3: value,})}/>
-                )}
-                            {renderIf(this.state.p4, 
+                  onSlidingComplete={value => this.setState({ v3: value,})}
+                />
+            )}
+
+            {renderIf(this.state.p4, 
                 <Slider
                   value={this.state.value}
-                    onSlidingComplete={value => this.setState({ v4: value,})}/>
-                )}
-                            {renderIf(this.state.p5, 
+                  onSlidingComplete={value => this.setState({ v4: value,})}
+                />
+            )}
+
+            {renderIf(this.state.p5, 
                 <Slider
                   value={this.state.value}
-                    onSlidingComplete={value => this.setState({ v5: value,})}/>
-                )}
-                            {renderIf(this.state.p6, 
+                  onSlidingComplete={value => this.setState({ v5: value,})}
+                />
+            )}
+
+            {renderIf(this.state.p6, 
                 <Slider
                   value={this.state.value}
-                    onSlidingComplete={value => this.setState({ v6: value,})}/>
-                )}
-                            {renderIf(this.state.p7, 
+                  onSlidingComplete={value => this.setState({ v6: value,})}
+                />
+            )}
+                          
+            {renderIf(this.state.p7, 
                 <Slider
                   value={this.state.value}
-                    onSlidingComplete={value => this.setState({ v7: value,})}/>
-                )}
-                            {renderIf(this.state.p8, 
+                  onSlidingComplete={value => this.setState({ v7: value,})}
+                />
+            )}
+
+            {renderIf(this.state.p8, 
                 <Slider
                   value={this.state.value}
-                    onSlidingComplete={value => this.setState({ v8: value,})}/>,
-                    
-                )}
-                            {renderIf(this.state.text,
+                  onSlidingComplete={value => this.setState({ v8: value,})}
+                />                    
+            )}
+                            
+            {renderIf(this.state.text,
                 <View style={styles.sliderF}>
-                <Text style={styles.containerProducts}>Least</Text>
-                <Text style={styles.containerProducts}>Most</Text>
+                  <Text style={styles.containerProducts}>Least</Text>
+                  <Text style={styles.containerProducts}>Most</Text>
                 </View>
-                 )}
-            </View> 
-<View style={styles.buttonContainer}>
-          <Button
-            title="Go back"
-            onPress={_.debounce(() => {this._onPress(screenId,bId=1)},400)}
-          />
-          <Text></Text>
-          <TouchableHighlight
-            underlayColor={'#0018A8'}
-            style={styles.buttonConfirm}
-            onPress={_.debounce(() => {this._onPress(screenId,bId=0)},400)}
+            )}
+
+          </View> 
+
+          <View style={styles.buttonContainer}>
+            <Button
+             title="Go back"
+             onPress={_.debounce(() => {this._onPress(screenId,bId=1)},400)}
+            />
+            <Text></Text>
+            <TouchableHighlight
+              underlayColor={'#0018A8'}
+              style={styles.buttonConfirm}
+              onPress={_.debounce(() => {this._onPress(screenId,bId=0)},400)}
             >
-
             <Text style={styles.btext}>Confirm</Text>
-
-          </TouchableHighlight>
-          
-
+            </TouchableHighlight>
           </View>
+
         </View>      
       ); //End of return
     } //End of render
@@ -326,14 +347,16 @@ import _, {debounce} from 'lodash';
       flex: 1,
       backgroundColor: 'white',
     },
+
     tContainer: {
       backgroundColor: 'white',
       alignItems: 'center',
       justifyContent: 'center',
       flexDirection: 'row',
       paddingBottom: 40,
-      paddingTop: 20
+      paddingTop: 40
     },
+
     bcontainer: {
       backgroundColor: 'white',
       alignItems: 'center',
@@ -341,8 +364,9 @@ import _, {debounce} from 'lodash';
       paddingVertical: 2,
       flexDirection: 'row',
     },
+
     buttonContainer: {
-      paddingBottom: 5,
+      paddingBottom: 15,
       paddingTop: 30,
       backgroundColor: 'white',
       alignItems: 'center',
@@ -350,13 +374,14 @@ import _, {debounce} from 'lodash';
       paddingVertical: 2,
       flexDirection: 'row',
     },
+
     sContainer: {
       flex: 1,
       marginLeft: 10,
       marginRight: 10,
       alignItems: "stretch",
       justifyContent: "center"
-  },
+    },
 
     btext: {
       color: 'white',
@@ -374,22 +399,25 @@ import _, {debounce} from 'lodash';
     },
     
     buttonS: {
-        alignItems: 'center',
-        backgroundColor: '#0018A8',
-      },
-      buttonConfirm: {
-        alignItems: 'center',
-        backgroundColor: '#0247FE',
-        padding: 10,
-        borderRadius: 400,
-      },
-      sliderF: {
-        flexDirection:'row',
-        justifyContent : 'space-between'
-      },
-      containerProducts: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        },
+      alignItems: 'center',
+      backgroundColor: '#0018A8',
+    },
+
+    buttonConfirm: {
+      alignItems: 'center',
+      backgroundColor: '#0247FE',
+      padding: 10,
+      borderRadius: 400,
+    },
+
+    sliderF: {
+      flexDirection:'row',
+      justifyContent: 'space-between'
+    },
+
+    containerProducts: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
       
   });
